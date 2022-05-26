35e6883313e41b78156a409396b941024fd0014a
Min <nichapha.t@reverseads.com>
2022-05-26 18:00:46 +0700

    change group name 'Process' to 'Reviewing'

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,16 +1,20 @@
 import React from "react";
 import '../App.css'
+import trashDefault from '../assets/trash-icon-default.png'
 import TaskPanel from '../components/TaskPanel'
 
-export default function Group({category,tasks,handleDragStart,handleDragOver,handleDrop,handleChange}) {
-
+export default function Group({ category, tasks, handleChange, handleDragStart, handleDragOver, handleDrop, toggleModal, addTask, editTask, deleteTask, input }) {
+  function toggleModal() {
+    setIsOpen(prev => !prev)
+  }
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
     <>    
       <div className=
         {`category-group 
           ${category === 'To-Do' ?'red-bg' :
             category === 'Doing' ? 'yellow-bg' :
-            category ==='Process'?  'olive-bg' : 'green-bg'
+            category ==='Reviewing'?  'olive-bg' : 'green-bg'
           }`}
         
         key={category}
@@ -27,15 +31,53 @@ export default function Group({category,tasks,handleDragStart,handleDragOver,han
             .map(task => {
             return (              
               <div className='task-note' key={task.id} id={task.id} draggable
-                   onDragStart={()=>handleDragStart(task.id,category)}>
-                <textarea className="task-title" name='title' onChange={handleChange} value=''>
+                onDragStart={() => handleDragStart(task.id, category)}>
+                
+                <textarea className="task-title" key={task.id} value={task.title} name='title'
+                  onChange={(event)=> editTask(task.id,event) }
+                  >
                   {task.title}
                 </textarea>
-                <textarea className="task-content" name='content'>{task.content}</textarea>
+
+                <textarea className="task-content" key={task.id} value={task.content} name='content'
+                onChange={(event) => editTask(task.id, event)} 
+                >{task.content}</textarea>
+                <img className='deleteBtn' onClick={() => deleteTask(task.id)} src={trashDefault}/>
             </div>
             )
-      })}
+            })}
+            </div>
+                {isOpen &&
+        <div className='overlay'> 
+                 <div className='modal'> 
+            <div id={category} className='note-form'>    
+            <input
+              name='title'
+              className="modal-title"
+              type="text"
+              placeholder="Title"
+              onChange={(e)=>handleChange(category,e)}
+              value={input.title||''}
+              >
+            </input>
+            <input
+              name='content'
+              className="modal-content"
+              type="text"
+              placeholder="Take a note"
+              onChange={(e)=>handleChange(category,e)}
+              value={input.content||''}
+              >
+              </input>
+              <button type='submit' onClick={addTask}> Submit </button>
+              <button onClick={() => { setIsOpen(false) }}> Close </button>
         </div>
+                  </div>
+                  </div>
+                  
+                  } 
+
+          <button key={category} className="addBtn" onClick={toggleModal}> + </button>
     </div>
     </>
   )
